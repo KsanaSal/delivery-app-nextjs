b959582029c33eeec920fa3cd19e8498596d8c49
KsanaSal <oasalivon@gmail.com>
2023-06-02 22:46:22 +0300

    Add global styles and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "sky-medium": "#4CB7FF",
+                "sky-black": "#032135",
+                "green-light": "#d2eff1",
+            },
+        },
+        fontFamily: {
+            sans: ["Georgia", "sans-serif"],
+            serif: ["Merriweather", "serif"],
+            display: ["Oswald"],
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
